9fbfc29fb6a98cf664d5740e9258ee03c94bca23
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-18 22:22:11 +0530

    11

---
+++ Assignment Question/11.js
@@ -0,0 +1,58 @@
+// Write a javascript code to create a class to represent a Student details include the Student Roll No, Name of the Student, Branch, year, location and college. Assign initial values using constructor. Calculate average of marks of 6 subjects and
+// calculate attendance percentage
+
+
+
+class Student {
+    constructor(rollNo, name, branch, year, location, college) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.branch = branch;
+        this.year = year;
+        this.location = location;
+        this.college = college;
+        this.marks = [];
+        this.attendance = [];
+    }
+
+    addMark(mark) {
+        this.marks.push(mark);
+    }
+
+    calculateAverage() {
+        const totalMarks = this.marks.reduce((sum, mark) => sum + mark, 0);
+        return totalMarks / this.marks.length;
+    }
+
+    addAttendance(isPresent) {
+        this.attendance.push(isPresent);
+    }
+
+    calculateAttendancePercentage() {
+        const totalDays = this.attendance.length;
+        const presentDays = this.attendance.filter(isPresent => isPresent).length;
+        return (presentDays / totalDays) * 100;
+    }
+}
+
+// Create student instance and add marks and attendance
+const student = new Student(101, "Aditya", "Computer Science", 2, "India", "MMEC College");
+student.addMark(85);
+student.addMark(90);
+student.addMark(78);
+student.addMark(92);
+student.addMark(88);
+student.addMark(95);
+
+student.addAttendance(true);
+student.addAttendance(true);
+student.addAttendance(false);
+student.addAttendance(true);
+student.addAttendance(true);
+student.addAttendance(true);
+student.addAttendance(false);
+
+// Calculate and display average marks and attendance percentage
+console.log(`Student - ${student.name}`);
+console.log(`Average Marks: ${student.calculateAverage()}`);
+console.log(`Attendance Percentage: ${student.calculateAttendancePercentage()}%`);
